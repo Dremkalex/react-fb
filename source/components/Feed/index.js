aeb8aecb8bc7abf58f88e412ead079c84efb7500
Mashchenko Oleksandra <dremka@hotmail.com>
2019-02-17 20:00:53 +0200

    Posts with animate. Using CSSTransition and TransitionGroup

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,7 +1,7 @@
 //Core
 import React, { Component } from 'react';
 import { string } from 'prop-types';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group';
 import { fromTo } from 'gsap';
 
 //Components
@@ -156,13 +156,27 @@ export default class Feed extends Component {
         const {posts, isPostFetching } = this.state;
         const postsJSX = posts.map((post) => {
             return (
-                <Catcher key = { post.id }>
-                    <Post
-                        { ...post }
-                        _likePost = { this._likePost }
-                        _removePost = { this._removePost }
-                    />
-                </Catcher>
+                <CSSTransition
+                    classNames = {{
+                        enter:       Styles.postInStart,
+                        enterActive: Styles.postInEnd,
+                        exit:        Styles.postOutStart,
+                        exitActive:  Styles.postOutEnd,
+                    }}
+                    key = { post.id }
+                    timeout = {{
+                        enter: 500,
+                        exit:  400,
+                    }}>
+                    <Catcher>
+                        <Post
+                            { ...post }
+                            _likePost = { this._likePost }
+                            _removePost = { this._removePost }
+                        />
+                    </Catcher>
+                </CSSTransition>
+
             );
         });
 
@@ -178,7 +192,7 @@ export default class Feed extends Component {
                     <Composer _createPost = { this._createPost }/>
                 </Transition>
                 <Postman />
-                {postsJSX}
+                <TransitionGroup>{postsJSX}</TransitionGroup>
             </section>
         );
     }
